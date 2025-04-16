44493e2c2f0c07244a46f5b3587758bc3c7da83d
Salif Biaye <salifbiaye@esp.sn>
2025-04-16 01:19:22 +0000

    Update training page title and add users page with basic structure

---
+++ src/app/(protected)/users/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur la page d'accueil</h1>
+            <p>users</p>
+        </div>
+    );
+};
+
+export default HomePage;
