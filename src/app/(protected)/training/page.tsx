ec48bcdab009df288a561ab03023f5d003fc7e37
Salif Biaye <salifbiaye@esp.sn>
2025-04-16 01:18:42 +0000

    Add training and users pages with basic structure and content in French. Each page includes a welcome message and a description, enhancing the application's multilingual support.

---
+++ src/app/(protected)/training/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Bienvenue sur la page d'accueil</h1>
+            <p>Calendar</p>
+        </div>
+    );
+};
+
+export default HomePage;
