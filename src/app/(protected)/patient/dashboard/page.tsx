0f6b4a8fa402afb48027af3ca360e4a3819209c9
Salif Biaye <salifbiaye@esp.sn>
2025-05-11 16:00:52 +0000

    Add new dashboard pages for admin, director, doctor, notifications, patient, and secretary roles with a welcome message. Remove the existing notifications page to streamline the application structure.

--- src/app/(protected)/patient/dashboard/page.tsx
+++ src/app/(protected)/patient/dashboard/page.tsx
@@ -5,7 +5,7 @@ const HomePage = () => {
     return (
         <div>
             <h1>Bienvenue sur la page d'accueil</h1>
-            <p>Calendar</p>
+            <p>Training</p>
         </div>
     );
 };
